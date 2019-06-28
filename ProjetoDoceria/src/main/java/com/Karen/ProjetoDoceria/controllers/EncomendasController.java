cb1da209d121d26ff694e3032dfb346d9da4ed45
Aluno <Aluno@172.30.1.51>
2019-06-27 22:32:19 -0300

    Alterações.

--- ProjetoDoceria/src/main/java/com/Karen/ProjetoDoceria/controllers/EncomendasController.java
+++ ProjetoDoceria/src/main/java/com/Karen/ProjetoDoceria/controllers/EncomendasController.java
@@ -11,7 +11,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.Karen.ProjetoDoceria.models.Cliente;
@@ -65,9 +64,5 @@ public class EncomendasController {
 		}
 		repositoryCliente.saveAndFlush(cliente);
 		return lista();
-	}
-	@RequestMapping("/cadastrarPedido")
-	public String cadastrarPedido() {
-		return"cadastrarPedido";
 	}
 }
